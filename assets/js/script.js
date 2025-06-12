55a5712107e36d4f060751d1004c4b671da50846
Alex-McAlpine <alexmcalpine7@hotmail.com>
2025-06-12 09:36:10 +0100

    ADDED: Java structure

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function rungame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswers() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswers() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
